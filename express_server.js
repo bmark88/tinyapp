345afad049d132aed83dfc2b43798c440004cc0f
bmark88 <bradley.mark@hotmail.com>
2020-03-16 23:23:27 +0000

    added /urls_new route path

--- express_server.js
+++ express_server.js
@@ -30,8 +30,12 @@ app.get("/urls", (req, res) => {
   res.render("urls_index", templateVars);
 });
 
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
   let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
   res.render("urls_show", templateVars);
-})
+});

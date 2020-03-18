17086ffae4c105a52c76fa6b8e8f727941a0c7e5
bmark88 <bradley.mark@hotmail.com>
2020-03-18 01:48:32 +0000

    added login/logout features with cookies

--- express_server.js
+++ express_server.js
@@ -5,21 +5,33 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+
+// const users = {
+//   "hello": {
+//     username: 'hello',
+//     password: 'password'
+//   }
+// };
+
 const bodyParser = require("body-parser");
+const cookieParser = require('cookie-parser');
 
 const generateRandomString = () => {
   const chars = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890';
   let random = '';
-  
+
   for (let i = 0; i < chars.length; i++) {
     if (random.length < 6) {
-      random += chars[(Math.random()*chars.length).toFixed(0)]
+      random += chars[(Math.random() * chars.length).toFixed(0)]
     }
   }
   return random;
 }
+app.use(cookieParser());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 
-app.use(bodyParser.urlencoded({extended: true}));
 
 app.set('view engine', 'ejs');
 
@@ -40,17 +52,27 @@ app.get("/hello", (req, res) => {
 });
 
 app.get("/urls", (req, res) => {
-  let templateVars = { urls: urlDatabase };
+  let templateVars = {
+    urls: urlDatabase,
+    username: req.cookies.username ? req.cookies.username : null
+  };
   res.render("urls_index", templateVars);
 });
 
 app.get("/urls/new", (req, res) => {
-  res.render("urls_new");
+  let templateVars = {
+    username: req.cookies.username ? req.cookies.username : null
+  };
+  res.render("urls_new", templateVars);
 });
 
 app.get('/urls/:shortURL', (req, res) => {
   const shortURL = req.params.shortURL;
-  let templateVars = { shortURL: req.params.shortURL, longURL: urlDatabase[shortURL] };
+  let templateVars = {
+    shortURL: req.params.shortURL,
+    longURL: urlDatabase[shortURL],
+    username: req.cookies.username ? req.cookies.username : null
+  };
   res.render("urls_show", templateVars);
 });
 
@@ -73,4 +95,20 @@ app.post("/urls/:shortURL/delete", (req, res) => {
 app.post("/urls/:shortURL", (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.input;
   res.redirect('/urls')
+});
+
+app.post("/login", (req, res) => {
+  let username = req.body.username;
+
+  if (username) {
+    res.cookie('username', username);
+    res.redirect('/urls');
+  } else {
+    res.send('There is no username');
+  }
+});
+
+app.post('/logout', (req, res) => {
+  res.clearCookie('username');
+  res.redirect('/urls');
 });
